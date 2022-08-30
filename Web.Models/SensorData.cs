656e2b79b0e2babb0c697afcf1f22ae04315b776
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-08-31 00:00:05 +0300

    seri log

---
+++ Web.Models/SensorData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web.Models
+{
+    public class SensorData
+    {
+        public int Id { get; set; }
+        public int Humidity { get; set; }
+        public int Temprature { get; set; }
+    }
+}
